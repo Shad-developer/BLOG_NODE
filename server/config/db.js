f3d1e0ab52ff1467355c51cccd3533e5c2ddcc8d
Umair Shad <151988665+Shad-developer@users.noreply.github.com>
2024-03-20 11:42:02 +0500

    db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async ()=>{
     try{
         mongoose.set('strictQuery', false);
-        const conn = await mongoose.connect(mongodb+srv://uch24502:umair24502@cluster0.kymyzez.mongodb.net/NodeJsBlog);
+        const conn = await mongoose.connect('mongodb+srv://uch24502:umair24502@cluster0.kymyzez.mongodb.net/NodeJsBlog');
         console.log(`Database Connected: ${conn.connection.host}`);
     }catch(err){
         console.log(err);
